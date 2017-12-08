7102cd2b943ad6b87008e4c7258544880195f1c4
Vinicius Tiesse <contato@deltacoding.xyz>
2017-12-08 09:45:57 -0200

    BookShelfChanger identation change.

--- src/components/BookShelfChanger/index.js
+++ src/components/BookShelfChanger/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import {SelectField, MenuItem} from 'material-ui';
+import {SelectField, MenuItem} from 'material-ui'
 import './BookShelfChanger.css'
 
 const BookShelfChanger = ({book, onBookShelfChange}) => {
   return (
     <div className="book-shelf-changer">
-      <SelectField className="book-shelf-changer-select"
-                   value={book.shelf}
-                   onChange={
-                     (event, index, value) => onBookShelfChange(book, value)
-                   }>
-        <MenuItem value="none" primaryText="Move to..." disabled />
-        <MenuItem value="currentlyReading" primaryText="Currently Reading" />
-        <MenuItem value="wantToRead" primaryText="Want to Read" />
-        <MenuItem value="read" primaryText="Read" />
-        <MenuItem value="none" primaryText="None" />
+      <SelectField
+        className="book-shelf-changer-select"
+        value={book.shelf}
+        onChange={
+          (event, index, value) => onBookShelfChange(book, value)
+        }
+      >
+        <MenuItem value="none" primaryText="Move to..." disabled/>
+        <MenuItem value="currentlyReading" primaryText="Currently Reading"/>
+        <MenuItem value="wantToRead" primaryText="Want to Read"/>
+        <MenuItem value="read" primaryText="Read"/>
+        <MenuItem value="none" primaryText="None"/>
       </SelectField>
     </div>
   )
