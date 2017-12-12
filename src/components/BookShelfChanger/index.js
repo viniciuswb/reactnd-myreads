57d5fd5307984f6d8a51ef481e4c2c10c0e10578
Vinicius Tiesse <contato@deltacoding.xyz>
2017-12-12 17:45:34 -0200

    Changed Select Field component for IconMenu to personalize the icon.

--- src/components/BookShelfChanger/index.js
+++ src/components/BookShelfChanger/index.js
@@ -1,32 +1,38 @@
 import React from 'react'
-import {SelectField, MenuItem, FloatingActionButton} from 'material-ui'
+import {IconMenu, MenuItem, FloatingActionButton} from 'material-ui'
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more'
 import './BookShelfChanger.css'
 
 const BookShelfChanger = ({book, onBookShelfChange}) => {
   return (
-    <FloatingActionButton
-      className="book-shelf-changer"
-      mini={true}
-      backgroundColor="#1e5a9f"
-    >
-      <div className="book-shelf-changer-icon">
-        <SelectField
-          className="book-shelf-changer-select"
-          autoWidth={true}
-          value={book.shelf}
-          onChange={
-            (event, index, value) => onBookShelfChange(book, value)
-          }
-          selectedMenuItemStyle={{color: '#1e5a9f'}}
-        >
-          <MenuItem value="none" primaryText="Move to..." disabled/>
-          <MenuItem value="currentlyReading" primaryText="Currently Reading"/>
-          <MenuItem value="wantToRead" primaryText="Want to Read"/>
-          <MenuItem value="read" primaryText="Read"/>
-          <MenuItem value="none" primaryText="None"/>
-        </SelectField>
-      </div>
-    </FloatingActionButton>
+    <div className="book-shelf-changer-select">
+      <IconMenu
+        style={{
+          position: 'absolute',
+          right: 0,
+          bottom: '-10px'
+        }}
+        iconButtonElement={
+          <FloatingActionButton
+            mini={true}
+            backgroundColor="#1e5a9f"
+          >
+            <NavigationExpandMoreIcon/>
+          </FloatingActionButton>
+        }
+        value={book.shelf}
+        onChange={
+          (event, value) => onBookShelfChange(book, value)
+        }
+        selectedMenuItemStyle={{color: '#1e5a9f'}}
+      >
+        <MenuItem value="none" primaryText="Move to..." disabled/>
+        <MenuItem value="currentlyReading" primaryText="Currently Reading"/>
+        <MenuItem value="wantToRead" primaryText="Want to Read"/>
+        <MenuItem value="read" primaryText="Read"/>
+        <MenuItem value="none" primaryText="None"/>
+      </IconMenu>
+    </div>
   )
 }
 
