9469bc618d7fca6b550c93b1b94fc9c63ae8c250
Vinicius Tiesse <contato@deltacoding.xyz>
2017-12-15 19:58:01 -0200

    Added rating of book using stars on book datails modal.

--- src/components/BookDetails/index.js
+++ src/components/BookDetails/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Dialog, FlatButton} from 'material-ui'
+import ReactStars from 'react-stars'
 import './BookDetails.css'
 
 class Modal extends Component {
@@ -27,7 +28,18 @@ class Modal extends Component {
         <div>
           <h3>{this.props.book.title}</h3>
           <h4>{this.props.book.subtitle}</h4>
-          <img src={this.props.book.imageLinks.thumbnail} alt="Book cover" />
+          <img className="book-detail-cover" src={this.props.book.imageLinks.thumbnail} alt="Book cover"/>
+          <div className="book-ratings">
+            <ReactStars
+              count={5}
+              value={this.props.book.averageRating}
+              size={18}
+              edit={false}
+              color2={'#ffd700'}
+              className="book-ratings-stars"
+            />
+            <p className="book-ratings-count">({this.props.book.ratingsCount ? this.props.book.ratingsCount : 0})</p>
+          </div>
         </div>
       </Dialog>
     )
