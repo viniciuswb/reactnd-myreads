27e5874a5c89d762fe521b228692734908427522
Vinicius Tiesse <contato@deltacoding.xyz>
2017-12-15 20:32:33 -0200

    A shadow was placed on the book cover on datails.

--- src/components/BookDetails/index.js
+++ src/components/BookDetails/index.js
@@ -1,49 +1,43 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Dialog, FlatButton} from 'material-ui'
 import ReactStars from 'react-stars'
 import './BookDetails.css'
 
-class Modal extends Component {
-  componentDidMount() {
-    console.log(this.props.book)
-  }
+const Modal = ({modalClose, modalOpen, book}) => {
+  const actions = [
+    <FlatButton
+      label="Close"
+      primary={true}
+      onClick={modalClose}
+    />
+  ]
 
-  render() {
-    const actions = [
-      <FlatButton
-        label="Close"
-        primary={true}
-        onClick={this.props.modalClose}
-      />
-    ]
-
-    return (
-      <Dialog
-        className="book-details"
-        actions={actions}
-        modal={true}
-        open={this.props.modalOpen}
-        autoScrollBodyContent={true}
-      >
-        <div>
-          <h3>{this.props.book.title}</h3>
-          <h4>{this.props.book.subtitle}</h4>
-          <img className="book-detail-cover" src={this.props.book.imageLinks.thumbnail} alt="Book cover"/>
-          <div className="book-ratings">
-            <ReactStars
-              count={5}
-              value={this.props.book.averageRating}
-              size={18}
-              edit={false}
-              color2={'#ffd700'}
-              className="book-ratings-stars"
-            />
-            <p className="book-ratings-count">({this.props.book.ratingsCount ? this.props.book.ratingsCount : 0})</p>
-          </div>
+  return (
+    <Dialog
+      className="book-details"
+      actions={actions}
+      modal={true}
+      open={modalOpen}
+      autoScrollBodyContent={true}
+    >
+      <div>
+        <h3>{book.title}</h3>
+        <h4>{book.subtitle}</h4>
+        <img className="book-detail-cover" src={book.imageLinks.thumbnail} alt="Book cover"/>
+        <div className="book-ratings">
+          <ReactStars
+            count={5}
+            value={book.averageRating}
+            size={18}
+            edit={false}
+            color2={'#ffd700'}
+            className="book-ratings-stars"
+          />
+          <p className="book-ratings-count">({book.ratingsCount ? book.ratingsCount : 0})</p>
         </div>
-      </Dialog>
-    )
-  }
+      </div>
+    </Dialog>
+  )
 }
 
 export default Modal
